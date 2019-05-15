5b28465acf0e3ec9b233c6874e7da9737643d07d
Simon Mercer <simon.mercer@clarksons.com>
2019-05-15 17:16:37 +0100

    blah

---
+++ XR_Wards/Models/RegisterModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using XR_Wards.CommonTypes;
+
+namespace XR_Wards.Models
+{
+    public class RegisterModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public Registration Registration { get; set; }
+
+        public void OnGet()
+        {
+            Message = "First load";
+        }
+
+        public void OnPost()
+        {
+            Registration = new Registration()
+            {
+                FirstName = Request.Form[nameof(Registration.FirstName)],
+                LastName = Request.Form[nameof(Registration.LastName)],
+                Email = Request.Form[nameof(Registration.Email)],
+                ConfirmStoringEmail = Request.Form[nameof(Registration.ConfirmStoringEmail)] == "true"
+            };
+        }
+    }
+}
